5e104642d354fa56f5d80bfba28b5827b2222293
Devika99 <52597709+Devika99@users.noreply.github.com>
2023-02-16 15:03:02 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,7 +1,7 @@
 public class a
 {
-public static void main(String[] args)
-{
-  System.out.println("Hello!");
-}
+ public static void main(String[] args)
+ {
+   System.out.println("Hello!");
+ }
 }
